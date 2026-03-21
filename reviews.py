8e2dd90fb1f7f3c54811e1f6d3d8a569f80b8b23
Vuorikallio Maria A A <vuomaria@cubbli24-gold.cs.helsinki.fi>
2026-03-21 18:01:23 +0200

    Show review list/info

--- reviews.py
+++ reviews.py
@@ -5,3 +5,17 @@ def new_review(title, author, description, user_id):
              VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, author, description, user_id])
     
+def get_reviews():
+    sql = "SELECT id, title FROM reviews ORDER BY id DESC"
+    return db.query(sql)
+    
+def get_review(review_id):
+    sql = """SELECT reviews.title,
+                    reviews.author,
+                    reviews.description,
+                    users.username
+             FROM reviews, users
+             WHERE reviews.user_id = users.id AND
+                   reviews.id =?"""
+    return db.query(sql, [review_id])[0]
+                          
